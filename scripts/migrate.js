186860518ba2b2908604749ffd7ad91afe8be18f
kevinfiol <me@kevinfiol.com>
2020-10-14 00:20:45 -0400

    migrate script to promise API

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -2,18 +2,11 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 const config = require ('../config.js');
 
-(async () => {
-    // open database
-    try {
-        const db = await open({
-            driver: sqlite3.Database,
-            ...config.database
-        });
-
-        await db.migrate({
-            force: true
-        });
-    } catch(e) {
-        console.error('Failed to migrate database!: ' + e.message);
-    }
-})();
+open({
+    driver: sqlite3.Database,
+    ...config.database
+}).then(async db => {
+    await db.migrate({ force: true })
+}).catch(e => {
+    console.error('Failed to migrate database!: ' + e.message);
+});
