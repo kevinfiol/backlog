937d64043b256b158f0a6e79951afe3621f313c7
kevinfiol <me@kevinfiol.com>
2020-10-20 17:40:47 -0400

    automate seeding

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,12 +4,35 @@ const config = require ('../config.js');
 const hasher = require('../src/util/hasher.js');
 const slugify = require('../src/util/slugify.js');
 
+const user = {
+    name: 'kevin',
+    password: 'hunter2',
+    lists: [
+        { listname: 'my backlog', userid: 1, sectionidOrder: '1,2' },
+        { listname: 'indie backlog', userid: 1, sectionidOrder: '4,3' }
+    ],
+    sections: [
+        { sectionname: 'list to complete', listid: 1, itemidOrder: '1,2' },
+        { sectionname: 'completed', listid: 1, itemidOrder: '4,3' },
+        { sectionname: 'indies to replay', listid: 2, itemidOrder: '6,5' },
+        { sectionname: 'indies completed', listid: 2, itemidOrder: '8,7' }
+    ],
+    items: [
+        { itemname: 'Final Fantasy II', url: 'https://www.igdb.com/games/final-fantasy-ii', sectionid: 1 },
+        { itemname: 'Kirbys Dreamland 3', url: null, sectionid: 1 },
+        { itemname: 'Doom', url: 'https://en.wikipedia.org/wiki/Doom_(1993_video_game)', sectionid: 2 },
+        { itemname: 'Super Mario Sunshine', url: null, sectionid: 2 },
+        { itemname: 'Factorio', url: 'https://www.igdb.com/games/factorio', sectionid: 3 },
+        { itemname: 'Bastion', url: 'https://www.igdb.com/games/bastion', sectionid: 3 },
+        { itemname: 'Dustforce', url: null, sectionid: 4 },
+        { itemname: 'Spelunky 2', url: null, sectionid: 4 }
+    ]
+}
+
 open({
     driver: sqlite3.cached.Database,
     ...config.database
 }).then(async db => {
-    // create dummy user and populate User table
-    const user = { name: 'kevin', password: 'hunter2' };
     const { hashed, salt } = hasher.hash(user.password);
 
     await db.run(`
@@ -25,32 +48,27 @@ open({
     await db.run(`
         INSERT INTO List (listname, slug, userid, sectionidOrder)
         VALUES 
-            ('my backlog', '${slugify('my backlog')}', 1, '1,2'),
-            ('indie backlog', '${slugify('indie backlog')}', 1, '4,3')
+        ${user.lists.map(list => `
+            ('${list.listname}', '${slugify(list.listname)}', ${list.userid}, '${list.sectionidOrder}')
+        `)}
     `);
 
     // create Sections for above lists
     await db.run(`
         INSERT INTO Section (sectionname, slug, listid, itemidOrder)
         VALUES
-            ('list to complete', '${slugify('list to complete')}', 1, '1,2'),
-            ('completed', '${slugify('completed')}', 1, '4,3'),
-            ('indies to replay', '${slugify('indies to replay')}', 2, '6,5'),
-            ('indies completed', '${slugify('indies completed')}', 2, '8,7')
+        ${user.sections.map(section => `
+            ('${section.sectionname}', '${slugify(section.sectionname)}', ${section.listid}, '${section.itemidOrder}')
+        `)}
     `);
 
     // create Items for above lists
     await db.run(`
         INSERT INTO Item (itemname, slug, url, sectionid)
         VALUES
-            ('Final Fantasy II', '${slugify('Final Fantasy II')}', 'https://www.igdb.com/games/final-fantasy-ii', 1),
-            ('Kirbys Dreamland 3','${slugify('Kirbys Dreamland 3')}', NULL, 1),
-            ('Doom', '${slugify('Doom')}', 'https://en.wikipedia.org/wiki/Doom_(1993_video_game)', 2),
-            ('Super Mario Sunshine', '${slugify('Super Mario Sunshine')}', NULL, 2),
-            ('Factorio', '${slugify('Factorio')}', 'https://www.igdb.com/games/factorio', 3),
-            ('Bastion', '${slugify('Bastion')}', 'https://www.igdb.com/games/bastion', 3),
-            ('Dustforce', '${slugify('Dustforce')}', NULL, 4),
-            ('Spelunky 2', '${slugify('Spelunky 2')}', NULL, 4)
+        ${user.items.map(item => `
+            ('${item.itemname}', '${slugify(item.itemname)}', ${item.url ? `'${item.url}'` : 'NULL'}, ${item.sectionid})
+        `)}
     `)
 }).catch(e => {
     console.error('Failed to seed database: ' + e.message);
