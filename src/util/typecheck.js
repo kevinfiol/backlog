9a15b16f23cb03aa68d85dc073565a554d8454e0
kevinfiol <me@kevinfiol.com>
2020-12-23 20:04:22 -0500

    better typecheck errors

--- src/util/typecheck.js
+++ src/util/typecheck.js
@@ -17,8 +17,25 @@ function typecheck(obj = {}) {
         const errors = [];
 
         function onError(type, x) {
+            if (errors.length < 1) {
+                console.error('\x1b[41m%s\x1b[0m', 'TypeErrors(s) occured:');
+                console.group();
+            }
             const error = TypeError(`${typeof x} should be ${type}`);
-            console.error(error);
+            const smallStack = error.stack
+                .split(' at ')
+                .filter(s => !/(TypeError)|(typecheck.js)|(node_modules)|(node:internal)/.test(s))
+                .map(function(s) {
+                    const filename = s.match(/([^/\\]*)\.(js)|(ts)/g)[0];
+                    let line = `> \x1b[43m${filename}\x1b[0m ${s.trim()}`;
+                    return line.split(' (').join('\n(');
+                })
+            ;
+
+            console.error('\x1b[41m%s\x1b[0m', error.message);
+            console.group();
+            console.error(smallStack.join('\n'));
+            console.groupEnd();
             errors.push(error);
         }
 
@@ -41,6 +58,7 @@ function typecheck(obj = {}) {
         }
 
         if (errors.length > 0) {
+            console.groupEnd();
             const error = TypeError('TypeError(s) occured');
             error.typeErrors = errors;
             throw error;
